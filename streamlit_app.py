98eb06a05161e63e9905bde8eb03e13329d393fa
Tony Kara <eniolaanthony@gmail.com>
2024-10-31 05:06:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('🎈 Enhanced-Disease-Prediction-Webapp')
 
 st.info('This App is built to predict diseases using various trained models')
 
-df = pd.read_csv('https://raw.githubusercontent.com/Tony-Kara/Enhanced-Disease-Prediction-Webapp/refs/heads/master/Dataset/heart.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/Tony-Kara/Enhanced-Disease-Prediction-Webapp/refs/heads/master/Dataset/heart.csv')
+  st.dataframe(df)
